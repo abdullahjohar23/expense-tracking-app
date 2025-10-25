3cb6666ae38251f00360cebecab3136e810fd7e7
Abdullah Johar <jrrabdullah23@gmail.com>
2025-10-25 11:01:44 +0600

    class to make the textstyle codes short

---
+++ lib/services/support_widget.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AppWidget {
+    static TextStyle headlineTextStyle(double textSize) {
+        return TextStyle(
+            color: Colors.black,
+            fontSize: textSize,
+            fontWeight: FontWeight.bold
+        );
+    }
+}
