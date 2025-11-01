1deb54adbf18064053e578f59ead80aa908ca5c7
Abdullah Johar <jrrabdullah23@gmail.com>
2025-11-01 12:03:43 +0600

    password template added

--- lib/services/reusable_designs.dart
+++ lib/services/reusable_designs.dart
@@ -59,3 +59,72 @@ class TextFieldDesign extends StatelessWidget {
         );
     }
 }
+
+class PasswordFieldDesign extends StatefulWidget {
+    final bool obsPass;
+    final String hintMessage;
+
+    const PasswordFieldDesign({
+        required this.obsPass,
+        required this.hintMessage,
+        super.key,
+    });
+
+    @override
+    State<PasswordFieldDesign> createState() => _PasswordFieldDesignState();
+}
+
+class _PasswordFieldDesignState extends State<PasswordFieldDesign> {
+    late bool _obscurePassword;
+
+    @override
+    void initState() {
+        super.initState();
+        _obscurePassword = widget.obsPass; // initialize local state
+    }
+
+    @override
+    Widget build(BuildContext context) {
+        final deviceWidth = MediaQuery.of(context).size.width;
+
+        return Container(
+            decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(10),
+            ),
+            
+            child: TextField(
+                obscureText: _obscurePassword,
+                decoration: InputDecoration(
+                    border: InputBorder.none,
+
+                    hintText: widget.hintMessage,
+                    hintStyle: TextStyle(
+                        fontSize: deviceWidth * 0.05,
+                        color: Colors.grey,
+                        fontWeight: FontWeight.w400,
+                    ),
+                    
+                    prefixIcon: Icon(
+                        Icons.lock,
+                        size: deviceWidth * 0.07,
+                        color: const Color(0xffdf815f),
+                    ),
+                    
+                    suffixIcon: IconButton(
+                        icon: Icon(
+                            _obscurePassword ? Icons.visibility_off : Icons.visibility,
+                            color: const Color(0xffdf815f),
+                        ),
+                        
+                        onPressed: () {
+                            setState(() {
+                                _obscurePassword = !_obscurePassword;
+                            });
+                        },
+                    ),
+                ),
+            ),
+        );
+    }
+}
