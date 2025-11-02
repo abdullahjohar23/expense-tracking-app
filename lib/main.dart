f0a060d2742247664d86d6dd3a6d77f81971ed5d
Abdullah Johar <jrrabdullah23@gmail.com>
2025-11-02 13:44:10 +0600

    code changed due to firebase adding

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
-import 'package:expense_tracking/pages/onboarding.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:expense_tracking/firebase_options.dart';
+import 'package:expense_tracking/pages/onboarding.dart';
 
-void main() {
+Future <void> main() async {
+    WidgetsFlutterBinding.ensureInitialized();
+    await Firebase.initializeApp(
+        options: DefaultFirebaseOptions.currentPlatform,
+    );
+
     runApp(const MyApp());
 }
 
