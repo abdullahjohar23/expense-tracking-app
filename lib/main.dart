792f906e59f05bed697b02f63887f64ae66226be
Abdullah Johar <jrrabdullah23@gmail.com>
2025-11-08 14:00:50 +0600

    white top bar for all pagese

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:expense_tracking/pages/home_page.dart';
 import 'package:expense_tracking/firebase_options.dart';
-import 'package:expense_tracking/pages/onboarding.dart';
+// import 'package:expense_tracking/pages/onboarding.dart';
 
 Future <void> main() async {
     WidgetsFlutterBinding.ensureInitialized();
     await Firebase.initializeApp(
         options: DefaultFirebaseOptions.currentPlatform,
+    );
+    
+    SystemChrome.setSystemUIOverlayStyle(
+        SystemUiOverlayStyle(
+            statusBarColor: Colors.white,
+            statusBarIconBrightness: Brightness.dark,
+        ),
     );
 
     runApp(const MyApp());
@@ -19,7 +28,7 @@ class MyApp extends StatelessWidget {
     Widget build(BuildContext context) {
         return MaterialApp(
             debugShowCheckedModeBanner: false,
-            home: Onboarding()
+            home: HomePage(),
         );
     }
 }
