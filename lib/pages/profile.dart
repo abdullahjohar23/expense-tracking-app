50c0a6b177d09dc13cb3aa2e0f177f65e2b5cc8c
Abdullah Johar <jrrabdullah23@gmail.com>
2025-11-08 13:35:08 +0600

    logout function added

--- lib/pages/profile.dart
+++ lib/pages/profile.dart
@@ -1,8 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:expense_tracking/pages/login.dart';
 import 'package:expense_tracking/services/reusable_designs.dart';
 
-class ProfilePage extends StatelessWidget {
+class ProfilePage extends StatefulWidget {
     const ProfilePage({super.key});
+
+  @override
+  State<ProfilePage> createState() => _ProfilePageState();
+}
+
+class _ProfilePageState extends State<ProfilePage> {
+    // Logout Function
+    Future<void> logout() async {
+        await FirebaseAuth.instance.signOut();
+        
+        // if (context.mounted) means “Is this widget still alive and connected to the screen?”
+        if (context.mounted) {
+            Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(builder: (context) => const LoginPage()),
+            );
+
+            ScaffoldMessenger.of(context).showSnackBar(
+                ScaffoldText.show('Successfully Logged Out!', Colors.green, context),
+            );
+        }
+    }
 
     @override
     Widget build(BuildContext context) {
@@ -15,7 +39,7 @@ class ProfilePage extends StatelessWidget {
             appBar: AppBar(
                 // backgroundColor: Colors.transparent, // make it overlay your background
                 backgroundColor: Colors.white,
-                elevation: 0, // remove shadow
+                elevation: 0, // removes shadow
                 
                 leading: IconButton(
                     icon: Icon(Icons.arrow_back, color: Colors.black),
@@ -67,7 +91,7 @@ class ProfilePage extends StatelessWidget {
                         ProfileInfoTile(text: 'Add Expense', prefixicon: Icons.payment, suffixicon: Icons.chevron_right),
 
                         // Logout
-                        ProfileInfoTile(text: 'Logout', prefixicon: Icons.logout, suffixicon: Icons.chevron_right),
+                        ProfileInfoTile(text: 'Logout', prefixicon: Icons.logout, suffixicon: Icons.chevron_right, onTap: logout,),
 
                         // Delete Account
                         ProfileInfoTile(text: 'Delete Account', prefixicon: Icons.delete, suffixicon: Icons.chevron_right),
