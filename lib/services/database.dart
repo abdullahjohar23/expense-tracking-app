a8235a0fcc34d5385ec606073b375b3ddf8ad93f
Abdullah Johar <jrrabdullah23@gmail.com>
2025-11-11 10:51:15 +0600

    database code to add user expense

--- lib/services/database.dart
+++ lib/services/database.dart
@@ -4,4 +4,8 @@ class DatabaseMethods {
     Future addUserInfo(Map<String, dynamic> userInfoMap, String id) async {
         await FirebaseFirestore.instance.collection('users').doc(id).set(userInfoMap);
     }
+
+    Future addUserExpense(Map<String, dynamic> userExpense, String id) async {
+        await FirebaseFirestore.instance.collection('users').doc(id).collection('Expense').add(userExpense);
+    }
 }
